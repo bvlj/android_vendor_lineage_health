0e69886989f479f6b135898bc75f61a9fbd9a9b9
Joey <joey@lineageos.org>
2020-11-29 11:01:49 +0100

    mod: use a contentProvider to initialize
    The onCreate method of a contentProvider
    is a more reliable place to load the native
    libs rather than the app component
    
    Signed-off-by: Joey <joey@lineageos.org>

---
+++ mod/src/main/java/org/lineageos/mod/health/providers/InitProvider.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2020 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.mod.health.providers
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import net.sqlcipher.database.SQLiteDatabase
+
+/**
+ * The purpose of this class is to initialize
+ * the sql cipher libraries
+ */
+internal class InitProvider : ContentProvider() {
+
+    override fun onCreate(): Boolean {
+        SQLiteDatabase.loadLibs(context?.applicationContext ?: context)
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<out String>?,
+        selection: String?,
+        selectionArgs: Array<out String>?,
+        sortOrder: String?
+    ): Cursor? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun getType(uri: Uri): String? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        throw UnsupportedOperationException()
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+        throw UnsupportedOperationException()
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<out String>?
+    ): Int {
+        throw UnsupportedOperationException()
+    }
+}
